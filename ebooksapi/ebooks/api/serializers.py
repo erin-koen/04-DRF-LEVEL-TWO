3098b48b9cbe5e7fdcc20a263b62b4bff0f4abb5
Erin Koen <erin.koen@gmail.com>
2019-08-29 16:59:32 -0400

    Done up to authentication

---
+++ ebooksapi/ebooks/api/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from ebooks.models import Ebook, Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Review
+        # need to exclude this for creation process...why?
+
+        exclude = ("ebook",)
+        # fields = "__all__"
+
+
+class EbookSerializer(serializers.ModelSerializer):
+    # make relationships between the two models explicit
+    reviews = ReviewSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Ebook
+        fields = "__all__"
---
+++ ebooksapi/ebooks/api/views.py
@@ -0,0 +1,50 @@
+from rest_framework import generics
+from rest_framework.generics import get_object_or_404
+from ebooks.models import Ebook, Review
+from ebooks.api.serializers import EbookSerializer, ReviewSerializer
+
+
+# with Concrete view classes, you get all of the commented out view in three lines.
+class EbookListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Ebook.objects.all()
+    serializer_class = EbookSerializer
+
+
+class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Ebook.objects.all()
+    serializer_class = EbookSerializer
+
+
+class ReviewCreateAPIView(generics.CreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    # to connect to an ebook instance, we need to overwrite perform_create()
+
+    def perform_create(self, serializer):
+        ebook_pk = self.kwargs.get("ebook_pk")
+        ebook = get_object_or_404(Ebook, pk=ebook_pk)
+        # link occurs below
+        serializer.save(ebook=ebook)
+
+
+class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+# in order to provide behavior functionalities to our classes, we need
+# to pass in our mixins with the genericAPIView class
+# class EbookListCreateAPIView(generics.GenericAPIView,
+#                              mixins.CreateModelMixin,
+#                              mixins.ListModelMixin):
+
+#     queryset = Ebook.objects.all()
+#     serializer_class = EbookSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         # convention that we use to tell our functions that they might need
+#         # to manage a variable number of arguments and kwargs without
+#         # needing to know exactly what it's going to require
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
